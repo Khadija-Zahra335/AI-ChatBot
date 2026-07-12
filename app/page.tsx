3e343d70c7c8ada352e71594d061e0cb14accfa4
Khadija Zahra <khadijazahra153@gmail.com>
2026-07-12 18:59:27 +0500

    Changed title heading in  page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         {/* Header */}
         <header className="py-5 border-b border-slate-700">
           <h1 className="text-2xl sm:text-3xl font-bold text-center">
-            Groq Chatbot
+            AI Chatbot
           </h1>
           <p className="text-center text-base sm:text-xl text-slate-400 mt-1">
             Powered by Llama on Groq
